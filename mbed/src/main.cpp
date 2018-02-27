a634b478948b60f231354979f9029c8eea74c9f4
Jimmy Beaudoin <jbeaudoin11@gmail.com>
2018-02-27 13:38:34 -0500

    final

--- mbed/src/main.cpp
+++ mbed/src/main.cpp
@@ -4,22 +4,30 @@
 
 AnalogIn data(p18);
 
-float height() {
-    float value = (float) data * 3.3f;
+double height() {
+    double value = (double) data * 3.3;
     return
-        -0.2752*pow(value, 5)
-        +2.2587*pow(value, 4)
-        -7.0954*pow(value, 3)
-        +10.7507*pow(value, 2)
-        -9.1581*pow(value, 1)
-        -4.748
+        +0.2752*pow(value, 5)
+        -2.2587*pow(value, 4)
+        +7.0954*pow(value, 3)
+        -10.7507*pow(value, 2)
+        +9.1581*value
+        -2.748
     ;
+    // return
+    //     -0.2752*pow(value, 5)
+    //     +2.2587*pow(value, 4)
+    //     -7.0954*pow(value, 3)
+    //     +10.7507*pow(value, 2)
+    //     -9.1581*value
+    //     +4.748
+    // ;
 }
 
 
 int main() {
     while(1) {
-        printf("%0.2f\n\r", height());
+        printf("%0.2fV -- %0.2fcm\n\r", (float) data * 3.3f, height());
 
         wait_ms(500);
     }
